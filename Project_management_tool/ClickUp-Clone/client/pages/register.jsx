d7c2ed70d54fd7bf235e31c27ff67d2e928b7a7d
Aksh Darji <akshdarji2019@gmail.com>
2023-08-09 16:57:59 +0530

    Project management tool added

---
+++ Project_management_tool/ClickUp-Clone/client/pages/register.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import createSection from "../containers/Card";
+import AuthForm from "../utils/AuthForm";
+import { registerAsync } from "../redux/slices/authSlice";
+
+const SignUpSection = createSection(AuthForm);
+
+function SignInPage() {
+  return (
+    <SignUpSection reducer={registerAsync} authType="register" />
+  );
+}
+
+export default SignInPage;
---
+++ Project_management_tool/ClickUp-Clone/client/utils/contexts/themeContext.jsx
@@ -0,0 +1,15 @@
+
+const themeColors = {
+  background: "#ffffff",
+  backgroundDark: "#e5e6e7",
+  // backgroundMedium: "#f6f2ff",
+  backgroundMedium: "#fafbfc",
+
+  textColor: "#292d34",
+  textBoldColor: "#53575e",
+
+  accentColor: "#ffa12f",
+  accentColorFaded: "#ffdbae",
+};
+
+export default themeColors;
